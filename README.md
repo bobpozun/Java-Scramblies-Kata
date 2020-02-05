f1fbb710011fd57be96ae3413ce46e667a20d86e
Robert Pozun <robert.pozun@paypal.com>
2020-02-05 12:30:32 -0500

    Base setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Java-Scramblies-Kata
+
+Base repo to solve https://www.codewars.com/kata/55c04b4cc56a697bb0000048/train/java 
---
+++ src/main/java/Scramblies.java
@@ -0,0 +1,8 @@
+public class Scramblies {
+
+    public static boolean scramble(String str1, String str2) {
+        boolean result = true;
+        // your code
+        return result;
+    }
+}
