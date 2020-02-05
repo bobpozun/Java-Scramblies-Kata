6223dafc98a101b8fcdd75c7f6053da3617b7d02
Robert Pozun <robert.pozun@paypal.com>
2020-02-05 12:43:07 -0500

    Split tests out quickly

--- src/test/java/ScrambliesTest.java
+++ src/test/java/ScrambliesTest.java
@@ -10,17 +10,43 @@ public class ScrambliesTest {
     }
 
     @Test
-    public void test() {
-        System.out.println("Fixed Tests scramble");
-        testing(Scramblies.scramble("rkqodlw","world"), true);
-        testing(Scramblies.scramble("cedewaraaossoqqyt","codewars"),true);
-        testing(Scramblies.scramble("katas","steak"),false);
+    public void test1() {
+        testing(Scramblies.scramble("rkqodlw", "world"), true);
+    }
+    @Test
+    public void test2() {
+         testing(Scramblies.scramble("cedewaraaossoqqyt","codewars"),true);
+    }
+    @Test
+    public void test3() {
+        testing(Scramblies.scramble("katas", "steak"), false);
+    }
+    @Test
+    public void test4() {
         testing(Scramblies.scramble("scriptjavx","javascript"),false);
+    }
+    @Test
+    public void test5() {
         testing(Scramblies.scramble("scriptingjava","javascript"),true);
+    }
+    @Test
+    public void test6() {
         testing(Scramblies.scramble("scriptsjava","javascripts"),true);
+    }
+    @Test
+    public void test7() {
         testing(Scramblies.scramble("javscripts","javascript"),false);
+    }
+    @Test
+    public void test8() {
         testing(Scramblies.scramble("aabbcamaomsccdd","commas"),true);
+    }
+    @Test
+    public void test9() {
         testing(Scramblies.scramble("commas","commas"),true);
+    }
+    @Test
+    public void test10() {
         testing(Scramblies.scramble("sammoc","commas"),true);
     }
 }
