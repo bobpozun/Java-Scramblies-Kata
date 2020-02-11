f6b358813c7f9c1190b6a80ef814d6428bd47f49
robertpozun <robert.pozun@gmail.com>
2020-02-11 15:51:49 +0000

    ima1

--- src/main/java/Scramblies.java
+++ src/main/java/Scramblies.java
@@ -3,6 +3,13 @@ public class Scramblies {
     public static boolean scramble(String str1, String str2) {
         boolean result = true;
         // your code
+        //int j = 0;
+        for(int i = 0; i < str2.length(); i++){
+            if(str1.contains(str2.charAt(i) + ""))
+                str1 = str1.substring(0, str1.indexOf(str2.charAt(i))) + str1.substring(str1.indexOf(str2.charAt(i)) + 1);
+            else
+                return false;
+        }
         return result;
     }
 }
